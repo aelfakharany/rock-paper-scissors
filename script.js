afd920a919d9552dd91eb2c676cd8ab2b2bf2f80
Ahmad El-Fakharany <ahmadel@umich.edu>
2024-12-26 20:37:55 -0500

    Add choice-getting functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('yooooo')
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3);
+    if (randomInt === 0) {
+        return "rock";
+    }
+    else if (randomInt === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let userInput = prompt("Enter your choice below!");
+    let choice = userInput.toLowerCase();
+    if (choice != "rock" && choice != "paper" && choice != "scissors") {
+        return null;
+    }
+    else {return choice;}
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
